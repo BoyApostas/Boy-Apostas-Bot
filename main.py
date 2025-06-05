fcad1732dc13c0b0015b861d8e21e7defb4b1195
Boy_Apostas <boyapostaspix@gmail.com>
2025-06-05 18:40:58 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -2,14 +2,12 @@
 import asyncio
 import logging
 from scheduler.daily_tasks import start_scheduler
-from bot.handlers import dp, bot
+
+logging.basicConfig(level=logging.INFO)
 
 async def main():
-    logging.basicConfig(level=logging.INFO)
-    start_scheduler()  # Inicia o agendador que roda as tarefas diárias (como as apostas às 07:00)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling()
+    logging.info("🚀 Iniciando Boy Apostas no Render (modo Background Worker)...")
+    start_scheduler()
 
 if __name__ == "__main__":
-    print("🚀 Iniciando Boy Apostas no Render (modo Background Worker)...")
     asyncio.run(main())
